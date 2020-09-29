a9a910bdf751ba4f10f2d19d868b749824adcc22
Emil Sayahi <em@emsa.cf>
2020-09-29 05:27:26 -0400

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -2,6 +2,7 @@ mkdir -p ${PACKAGES}
 cd ${PACKAGES}
 
 # TODO: Download zlib source tarball, uncompress tarball, change directory into it
+echo "Building zlib-${ZLIB_VERSION}"
 wget https://zlib.net/zlib-${ZLIB_VERSION}.tar.xz -q > /dev/null
 tar -xJf zlib-${ZLIB_VERSION}.tar.xz
 cd zlib-${ZLIB_VERSION}
@@ -11,9 +12,10 @@ make && make DESTDIR=${PACKAGES}/ install
 cd ../
 
 # TODO: Download Pacman source tarball, uncompress tarball, change directory into it
+echo "Building pacman-${PACMAN_VERSION}"
 wget https://sources.archlinux.org/other/pacman/pacman-${PACMAN_VERSION}.tar.gz -q > /dev/null
 tar -xf pacman-${PACMAN_VERSION}.tar.gz
 cd pacman-${PACMAN_VERSION}
-./configure --prefix=/usr --shared
+./configure
 make && make DESTDIR=${PACKAGES}/ install
 cd ../

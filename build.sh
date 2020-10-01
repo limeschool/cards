87cd9183d7d611359cf6f3f53c3c750d145d9c39
Emil Sayahi <em@emsa.cf>
2020-10-01 12:18:46 -0400

    Update build.yml, build-cards.sh, and 2 more files...

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export PROFILE=~/cards-profile
+set +h
+umask 0022 # Correct file permissions
+
+pacman -S archiso
+cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
+echo "packages.x86_64:\n---"
+echo "$(<${PROFILE}/packages.x86_64)"
+echo "---"
+mkdir ./out
+mkdir /tmp/archiso-tmp
+mkarchiso -v -w /tmp/archiso-tmp ${PROFILE}
+rm -rf /tmp/archiso-tmp
+mv ./out/archlinux.*.iso ~/cards.iso

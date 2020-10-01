b04b9e7f55d046c6e9b3484631ef89bc96374261
Emil Sayahi <em@emsa.cf>
2020-10-01 13:21:20 -0400

    Update build.yml and build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ umask 0022 # Correct file permissions
 pacman -Syu archiso --noconfirm
 cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
 
-sudo tee -a ${PROFILE}/packages.x86_64 > /dev/null <<EOT
+tee -a ${PROFILE}/packages.x86_64 > /dev/null <<EOT
 xorg
 mesa
 xf86-video-amdgpu

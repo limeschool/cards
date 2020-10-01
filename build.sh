2961f717fb484942317ffc255ab58c20c238149f
Emil Sayahi <em@emsa.cf>
2020-10-01 12:40:03 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,20 @@ umask 0022 # Correct file permissions
 
 pacman -Syu archiso --noconfirm
 cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
-echo "packages.x86_64:\n---"
+
+echo -e "packages.x86_64:\n---"
 echo "$(<${PROFILE}/packages.x86_64)"
 echo "---"
+echo -e "build.sh:\n---"
+echo "$(<${PROFILE}/build.sh)"
+echo "---"
+chmod +x ${PROFILE}/build.sh
+echo -e "Help:\n---"
+echo ${PROFILE}/build.sh -h
+echo "---"
+
 mkdir ./out
 mkdir /tmp/archiso-tmp
 mkarchiso -v -w /tmp/archiso-tmp ${PROFILE}
 rm -rf /tmp/archiso-tmp
-mv ./out/archlinux.*.iso ~/cards.iso
+mv ./out/archlinux-*-x86_64.iso ~/cards.iso

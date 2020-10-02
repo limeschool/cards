85e0d55ba435382bc4b8ba0175eac7e07c12d487
Emil Sayahi <em@emsa.cf>
2020-10-01 20:03:36 -0400

    Update build.yml, build.sh, and 17 more files...

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ umask 0022 # Correct file permissions
 
 pacman -Syu archiso --noconfirm
 cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
-cp -rf ./custom/. ${PROFILE}
+cp -rf ./cards/. ${PROFILE}
 
 tee -a ${PROFILE}/packages.x86_64 > /dev/null <<EOT
 xorg
+xorg-drivers
+xorg-server
 xorg-xinit
+xf86-input-libinput
 lightdm
 mesa
 gtk-engine-murrine
@@ -30,8 +33,9 @@ echo -e "packages.x86_64:\n---"
 echo "$(<${PROFILE}/packages.x86_64)"
 echo "---"
 
+ln -s /lib/systemd/system/lightdm.service ${PROFILE}/airootfs/etc/systemd/system/display-manager.service
+
 mkdir ./out
 mkdir /tmp/archiso-tmp
 mkarchiso -v -w /tmp/archiso-tmp ${PROFILE}
 rm -rf /tmp/archiso-tmp
-mv ./out/archlinux-*-x86_64.iso ~/cards.iso
---
+++ cards/profiledef.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="cards"
+iso_label="CARDS_$(date +%Y%m)"
+iso_publisher="Cards"
+iso_application="Cards Live Environment"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"

d03be0520d459f19f5b0a9c7e4308eb3ac97d888
Emil Sayahi <em@emsa.cf>
2020-10-05 15:33:02 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,7 +48,8 @@ pacman -Rdd gsettings-desktop-schemas
 #su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview ttf-raleway elementary-wallpapers-git \
 #gnome-doc-utils libhandy1 pantheon-default-settings pantheon-session-git switchboard-plug-elementary-tweaks-git pantheon-screencast \
 #pantheon-system-monitor-git elementary-planner-git"
-su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview pantheon-dpms-helper-git pantheon-git-meta \
+su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview elementary-wallpapers-git \
+pantheon-default-settings-git pantheon-notifications-git pantheon-session-git switchboard-plug-elementary-tweaks-git \
 pantheon-screencast pantheon-system-monitor-git elementary-planner-git ttf-raleway"
 
 echo -e "LOCAL_REPO:\n---"
@@ -149,61 +150,24 @@ vala
 ## VirtualBox
 virtualbox-guest-utils
 
-## AUR
-#ttf-raleway
-##gnome-settings-daemon-elementary
-#elementary-wallpapers-git
-#pantheon-default-settings
-#pantheon-session-git
-#switchboard-plug-elementary-tweaks-git
-#pantheon-screencast
-#pantheon-system-monitor-git
-##pantheon-mail-git # AUR package depends on "libhandy-1", not "libhandy1", which exists
-#elementary-planner-git
-contractor-git
-elementary-icon-theme-git
+## Pantheon (part 2)
+cups
+cups-pk-helper
+gvfs
+gvfs-afc
+gvfs-mtp
+gvfs-nfs
+gvfs-smb
+pulseaudio-bluetooth
+pantheon
+pantheon-unstable
+light-locker
+switchboard-plug-security-privacy
 elementary-wallpapers-git
-gala-git
-granite-git
-gtk-theme-elementary-git
-lightdm-pantheon-greeter-git
-pantheon-applications-menu-git
-pantheon-calculator-git
-pantheon-calendar-git
-pantheon-code-git
 pantheon-default-settings-git
-pantheon-dpms-helper-git
-pantheon-files-git
-pantheon-music-git
-pantheon-screenshot-git
+pantheon-notifications-git
 pantheon-session-git
-pantheon-videos-git
-switchboard-git
-switchboard-plug-a11y-git
-switchboard-plug-about-git
-switchboard-plug-applications-git
-switchboard-plug-datetime-git
-switchboard-plug-desktop-git
-switchboard-plug-display-git
-switchboard-plug-keyboard-git
-switchboard-plug-locale-git
-switchboard-plug-mouse-touchpad-git
-switchboard-plug-network-git
-switchboard-plug-notifications-git
-switchboard-plug-parental-controls-git
-switchboard-plug-power-git
-switchboard-plug-printers-git
-switchboard-plug-security-privacy-git
-switchboard-plug-sharing-git
-switchboard-plug-user-accounts-git
-wingpanel-indicator-bluetooth-git
-wingpanel-indicator-datetime-git
-wingpanel-indicator-network-git
-wingpanel-indicator-nightlight-git
-wingpanel-indicator-notifications-git
-wingpanel-indicator-power-git
-wingpanel-indicator-session-git
-wingpanel-indicator-sound-git
+switchboard-plug-elementary-tweaks-git
 pantheon-screencast
 pantheon-system-monitor-git
 elementary-planner-git

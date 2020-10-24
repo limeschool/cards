923171b4566241a103d55da647a7cb919c74df72
Emil Sayahi <em@emsa.cf>
2020-10-23 22:20:32 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ systemd-machine-id-setup # Prevents errors when building AUR packages
 
 pacman -Syu archiso git base-devel jq expac diffstat pacutils wget devtools libxslt cmake \
 intltool gtk-doc gobject-introspection gnome-common polkit dbus-glib libhandy \
-meson vala gnome-settings-daemon libgee --noconfirm --noprogressbar # Install packages we'll need to build
+meson vala gnome-settings-daemon libgee editorconfig-core-c gtksourceview4 \
+libdazzle vte3 --noconfirm --noprogressbar # Install packages we'll need to build
 
 # Allow us to use a standard user account w/ password-less sudo privilege (for building AUR packages later)
 tee -a /etc/sudoers > /dev/null <<EOT
@@ -43,6 +44,7 @@ su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --
 ttf-raleway \
 ttf-twemoji-color \
 libhandy1 \
+marble-gtk-git \
 pantheon-screencast \
 yay \
 gdm-plymouth \
